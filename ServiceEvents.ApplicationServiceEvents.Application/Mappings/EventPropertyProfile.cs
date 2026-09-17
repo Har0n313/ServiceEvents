16f6e3a9fc8a3817e82ebd9aee98876eff0fab82
Har0n313 <144205713+Har0n313@users.noreply.github.com>
2026-09-17 13:47:33 +0300

    add Event to EventResponse mapping

---
+++ ServiceEvents.ApplicationServiceEvents.Application/Mappings/EventPropertyProfile.cs
@@ -0,0 +1,17 @@
+﻿using ServiceEvents.Application.DTOs.EventPropertyDTO;
+using ServiceEvents.Domain.Entities;
+
+namespace ServiceEvents.Application.Mappings;
+
+public static class EventPropertyProfile
+{
+    public static EventPropertyResponse ToResponse(
+        this EventProperty entity)
+    {
+        return new EventPropertyResponse(
+            entity.Id,
+            entity.Name,
+            entity.DataType,
+            entity.IsActive);
+    }
+}
